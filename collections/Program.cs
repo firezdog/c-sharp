5f2c0ead9fee01932f1071dafd1ccba870970a82
firezdog@yahoo.com <firezdog@yahoo.com>
2018-05-01 15:28:45 -0500

    ...

--- collections/Program.cs
+++ collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace collections
 {
@@ -40,6 +41,28 @@ namespace collections
             //     }
             //     Console.Write(Environment.NewLine);
             // }
+
+            //Create a list of 5 things, output length, output and remove third item, output length.
+            // List<object> games = new List<object> {"Dark Souls 1", "Dark Souls 2", "Dark Souls 3", "Bloodborne", "Demon Souls"};
+            // Console.WriteLine(games.Count);
+            // Console.WriteLine(games[2]);
+            // games.Remove(games[2]);
+            // Console.WriteLine(games.Count);
+
+            //String-string dictionary, add names from array as keys with null value, then select random item from previous list as value. Print results.
+            string[] nameArray = {"Tim", "Martin", "Nikki", "Sara"};
+            List<object> games = new List<object> {"Dark Souls 1", "Dark Souls 2", "Dark Souls 3", "Bloodborne", "Demon Souls"};
+            Dictionary<string,string> dict = new Dictionary<string,string>();
+            Random ayn = new Random();
+            foreach (string name in nameArray) {
+                dict.Add(name, null);
+            }
+            List<string> keys = new List<string>(dict.Keys);
+            foreach (string key in keys){
+                int rand = ayn.Next(0,games.Count-1);
+                dict[key] = games[rand] as string;
+                Console.WriteLine("Key: {0}, Entry: {1}", key, dict[key]);
+            }
         }
     }
 }
