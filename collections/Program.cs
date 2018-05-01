ba9f135093104b561e96d24ebc838399ed4c9594
firezdog@yahoo.com <firezdog@yahoo.com>
2018-05-01 10:52:09 -0500

    ...

---
+++ collections/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Three Basic Arrays
+            // (1) Hold integers from 0 to 9
+            // (2) Names "Tim", "Martin", "Nikki", "Sara"
+            // (3) Alternate between true and false for ten values
+            // int[] numArray = {0,1,2,3,4,5,6,7,8,9};
+            // string[] nameArray = {"Tim", "Martin", "Nikki", "Sara"};
+            // bool[] boolArray = new bool[10];
+            // for (int i = 0; i < boolArray.Length; i++) {
+            //     if (i % 2 == 0) {
+            //         boolArray[i] = true;
+            //     } else {
+            //         boolArray[i] = false;
+            //     }
+            // }
+            // foreach (bool item in boolArray)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            // Generate a 2 dimensional array representing the multiplication tables.
+            // Setup the first row.
+            // int [,] multTable = new int[10,10];
+            // for (int i = 0; i < 10; i++) {
+            //     for (int j = 0; j < 10; j++) {
+            //         multTable[i,j] = (i+1) * (j+1);
+            //     }
+            // }
+            // // Print the damn thing!
+            // for (int i = 0; i < 10; i++) {
+            //     for (int j = 0; j < 10; j++) {
+            //         Console.Write(string.Format("{0} ", multTable[i,j]));
+            //     }
+            //     Console.Write(Environment.NewLine);
+            // }
+        }
+    }
+}
