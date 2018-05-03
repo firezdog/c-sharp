3f927cf322dc88ee72aab2f3bc1ee524d2707f03
Alex <firezdog@yahoo.com>
2018-05-03 18:46:12 -0500

    ...

---
+++ ASP.NET Core/FirstProject/Controllers/HelloController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers {
+    public class HelloController: Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public JsonResult Example() {
+            var anonObject = new {
+                firstName = "Alex",
+                lastName = "Leibowitz",
+                Age = 34,
+                state = "apologizes to Tom"
+            };
+            return Json(anonObject);
+        }
+        [HttpGet]
+        [Route("index")]
+        public string Index()
+        {
+            return "You've reached the Hello Controller!";
+        }
+        [HttpGet]
+        [Route("template/{name}")]
+        public string IActionResult(string Name) {
+            return $"Hello {Name}!";
+        }
+        [HttpPost]
+        [Route("index")]
+        public void IActionResult2() {
+            //Return a view
+        }
+    }
+}
