32727290736ac96e74a0c5665297b77323bd2959
Alex <firezdog@yahoo.com>
2018-05-04 12:49:20 -0500

    ...

---
+++ ASP.NET Core/Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+ 
+namespace Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("/projects")]
+        public IActionResult Projects(){
+            return View();
+        }
+        [HttpGet]
+        [Route("/contact")]
+        public IActionResult Contact(){
+            return View();
+        }
+    }
+}
