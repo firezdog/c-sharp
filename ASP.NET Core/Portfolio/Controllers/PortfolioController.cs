75b4bd6b4802fdf3b43e131daba516a1b85bc027
Alex <firezdog@yahoo.com>
2018-05-06 13:21:10 -0500

    ...

--- ASP.NET Core/Portfolio/Controllers/PortfolioController.cs
+++ ASP.NET Core/Portfolio/Controllers/PortfolioController.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System;
  
 namespace Portfolio.Controllers
 {
+    public class SecondController : Controller
+    {
+        [HttpGet]
+        [Route("demo")]
+        public string OtherMethod(string food, int quantity){
+            return $"You want {quantity} {food}s";
+        }
+    }
     public class PortfolioController : Controller
     {
         [HttpGet]
@@ -25,11 +34,21 @@ namespace Portfolio.Controllers
         [HttpPost]
         [Route("contact")]
         public IActionResult Contact(string name, string location, string language, string comment) {
-            ViewBag.name = name;
-            ViewBag.location = location;
-            ViewBag.language = language;
-            ViewBag.comment = comment;
-            return View("Comment");
+            return RedirectToAction("Comment", new {Name = name, Location = location, Language = language, Comment = comment});
+        }
+        [HttpGet]
+        [Route("comment")]
+        public IActionResult Comment(string Name, string Location, string Language, string Comment) {
+            ViewBag.name = Name;
+            ViewBag.Location = Location;
+            ViewBag.Language = Language;
+            ViewBag.Comment = Comment;
+            return View();
+        }
+        [HttpGet]
+        [Route("demo/{Food}/{Quantity}")]
+        public IActionResult Method(string Food, int Quantity) {
+            return RedirectToAction("OtherMethod", "Second", new {food = Food, quantity = Quantity});
         }
     }
 }
