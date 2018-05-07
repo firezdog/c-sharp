c83f07384d52a20163eef117cca7820428f849bb
Alex <firezdog@yahoo.com>
2018-05-07 17:25:59 -0500

    ...

---
+++ Intermediate ASP.NET Core/AjaxNotes/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AjaxNotes.Models;
+using DbConnection;
+
+namespace AjaxNotes.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        [Route("note")]
+        public IActionResult newNote(string title){
+            List<string> errors = new List<string>();
+            //Length validation.
+            if (title == null) {
+                errors.Add("You must enter a title.");
+                return Json(new {errors = errors});
+            } else if (title.Length < 4) {
+                errors.Add("Your title must be at least 4 characters.");
+                return Json(new {errors = errors});
+            }
+            //Uniqueness validation.
+            string query = $"SELECT * FROM notes WHERE title='{title}'";
+            if (DbConnector.Query(query).Count > 0) {
+                errors.Add("Note already exists.");
+                return Json(new {errors = errors});
+            }
+            query = $"INSERT INTO notes (title) VALUES ('{title}')";
+            DbConnector.Execute(query);
+            return RedirectToAction("getNotes");
+        }
+        public IActionResult getNotes() {
+            string query = "SELECT * FROM notes ORDER BY created_at DESC";
+            var result = DbConnector.Query(query);
+            var jresult = Json(result);
+            return jresult;
+        }
+        [HttpPost]
+        [Route("note/{id}")]
+        public IActionResult updateNote(string id, string description){
+            string query;
+            query = $"UPDATE notes SET description='{description}' WHERE id='{id}'";
+            DbConnector.Execute(query);
+            return RedirectToAction("getNotes");
+        }
+        [HttpPost]
+        [Route("note/{id}/delete")]
+        public IActionResult deleteNote (string id) {
+            string query;
+            query = $"DELETE FROM notes WHERE id='{id}'";
+            DbConnector.Execute(query);
+            return RedirectToAction("getNotes");
+        }
+        public IActionResult Index()
+        {
+            string query = "SELECT * FROM notes ORDER BY created_at DESC";
+            ViewBag.notes = DbConnector.Query(query);
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Intermediate ASP.NET Core/AjaxNotes/wwwroot/js/site.js
@@ -0,0 +1,81 @@
+﻿$(document).ready(function(){
+    $("#newNoteErrors").hide();
+    
+    //Create a new note.
+    $("#submitNote").click(function(e){
+        e.preventDefault();
+        $.post("note", {title: $("#form_title").val()}, function(data){
+            if(data["errors"]){
+                $("#newNoteErrors").show();
+                handleErrors(data);
+            }
+            else {
+                $("#newNoteErrors").hide();
+                $("#newNote").trigger("reset");
+                displayNotes(data);
+            }
+        });
+    })
+
+    //Delete a note.
+    $("#allNotes").on("click",".delete",function(e){
+        let id = $(e.currentTarget).attr('data');
+        $.post(`note/${id}/delete`, {id: id}, function(data){
+            displayNotes(data);
+        });
+    });
+
+    //Display form to update note.
+    $("#allNotes").on("click","p",function(e){
+        let id = $(e.currentTarget).attr('data');
+        $(this).parent().html(`
+            <div class="form-group">
+                <label for="textarea${id}">Note description:</label>
+                <textarea id="textarea${id}" class="form-control"></textarea>
+            </div>
+            <button data="${id}" class="float-right update btn btn-primary">Update</button>
+            `);
+    });
+
+    //Update a note.
+    $("#allNotes").on("click",".update",function(e){
+        let id = $(e.currentTarget).attr('data');
+        let description = $(`#textarea${id}`).val();
+        $.post(`note/${id}`, {description: description}, function(data){
+            displayNotes(data);
+        });
+    });
+
+});
+
+function displayNotes(data) {
+    let notes = "";
+    for (note of data) {
+        let description = note.description;
+        if (typeof description == "object"){
+            description = "";
+        }
+        // if (!note.description.Data){
+        //     note.description = "";
+        // }
+        notes += 
+        `<div class="note card">
+            <div class="card-header">
+                <h3 class="float-left">${note.title}</h3>
+                <button data=${note.id} class="delete btn btn-danger float-right">Delete</button>
+            </div>
+            <div class="card-body">
+                <p data=${note.id}>${description}</p>
+            </div>
+        </div>`
+    }
+    $("#allNotes").html(notes);
+}
+
+function handleErrors(data) {
+    let messages = "";
+    for (let error of data["errors"]){
+        messages += `<li class="list-group-item">${error}</li>`
+    }
+    $("#newNoteErrorList").html(messages);
+}
