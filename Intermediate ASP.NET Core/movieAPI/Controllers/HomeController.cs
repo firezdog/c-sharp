2a511cc25d87babe9275841bba999af70dba5f0f
Alex <firezdog@yahoo.com>
2018-05-08 15:33:35 -0500

    ...

--- Intermediate ASP.NET Core/movieAPI/Controllers/HomeController.cs
+++ Intermediate ASP.NET Core/movieAPI/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using movieAPI.Models;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
 
 namespace movieAPI.Controllers
 {
@@ -15,12 +18,46 @@ namespace movieAPI.Controllers
         private readonly IOptions<MyApiOptions> MyApiConfig;
         public HomeController(DbConnector connect, IOptions<MyApiOptions> apiConfig)
         {
-            _dbConnector = connect;
             MyApiConfig = apiConfig;
+            _dbConnector = connect;
+        }
+        [HttpGet]
+        [Route("search")]
+        public IActionResult Search(string search) {
+            JObject searchData = new JObject();
+            string ApiKey = MyApiConfig.Value.Key;
+            string url = $"https://api.themoviedb.org/3/search/movie?api_key={ApiKey}&language=en-US&query={search}&page=1&include_adult=false";
+            WebRequest.UseMovieApi(url, response => {
+                searchData = response;
+            }).Wait();
+            JToken firstMovie = new JObject();
+            try {
+                firstMovie = searchData["results"][0];
+            }
+            catch {
+                return Json(new {error="No result found."});
+            }
+            string title = firstMovie["title"].ToString();
+            string released;
+            string rating;
+            if (firstMovie["release_date"].ToString().Length > 0) {
+                released = firstMovie["release_date"].ToString();
+            } else {
+                released = "none";
+            }
+            if (firstMovie["vote_average"].ToString().Length > 0) {
+                rating = firstMovie["vote_average"].ToString();
+            } else {
+                rating = "none";
+            }
+            string query = $"INSERT INTO movies (title, released, rating) VALUES ('{title}','{released}','{rating}')";
+            _dbConnector.Execute(query);
+            return Json(_dbConnector.Query("SELECT * FROM movies ORDER BY created_at DESC"));
         }
         public IActionResult Index()
         {
-            string temp = MyApiConfig.Value.Key;
+            ViewData["Title"] = "Main";
+
             return View();
         }
         public IActionResult Error()
--- Intermediate ASP.NET Core/movieAPI/wwwroot/js/site.js
+++ Intermediate ASP.NET Core/movieAPI/wwwroot/js/site.js
@@ -1,1 +1,49 @@
-﻿// Write your JavaScript code.
+﻿$(document).ready(function(){
+    $("#error").hide();
+    $("#searchForm").submit(function(e){
+        e.preventDefault();
+        let that = this;
+        $.get("search", $(that).serializeObject(), function(data){
+            $("#searchForm").trigger("reset");
+            if(data.error){
+                $("#error").show();
+                $("#error").html(`${data.error}`);
+            } else {
+                $("#error").hide()
+                displayResults(data);
+            }
+        });
+    });
+});
+
+function displayResults(data){
+    let movieList = "";
+    data.forEach(movie => {
+        movieList += `
+        <tr>
+            <td>${movie["title"]}</td>
+            <td>${movie["released"]}</td>
+            <td>${movie["rating"]}</td>
+        </tr>
+        `
+    });
+    $("tbody").html(movieList);   
+}
+
+ //The function is based on http://css-tricks.com/snippets/jquery/serialize-form-to-json/
+
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
